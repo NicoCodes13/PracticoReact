9b8fdae1f538118ed685b843683e1c00ef7088ee
Nicolas Castañeda <nickcar13@gmail.com>
2020-09-16 01:58:01 -0500

    Custom hooks

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,37 +1,31 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
 
 import '../assets/styles/App.scss';
 
+const API = 'http://localhost:3000/initalState';
+
 const App = () => {
-  const [videos, setVideos] = useState({
-    mylist: [],
-    trends: [],
-    originals: [],
-  });
-
-  useEffect(() => {
-    fetch('http://localhost:3000/initalState')
-      .then((responese) => responese.json())
-      .then((data) => setVideos(data));
-  }, []);
-
-  console.log(videos);
+  const [videos, categories] = useInitialState(API);
 
   return (
     <div className='app'>
       <Header />
       <Search />
+
       {videos.mylist.lenght > 0 && (
         <Categories title='Mi lista'>
           <Carousel>
-            <CarouselItem />
+            {videos.trends.map((item) => (
+              <CarouselItem key={item.id} {...item} />
+            ))}
           </Carousel>
         </Categories>
       )}
